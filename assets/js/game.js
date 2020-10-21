68623baa68b8d47866f4a315f28be7ece48d4a1d
unknown <aaronthesecond@yahoo.com>
2020-10-21 17:24:46 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,18 +27,17 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
-      console.log(
-        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-      );
-  
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      
+      enemyHealth = Math.max(0, enemyHealth - damage);
+      
       // check enemy's health
       if (enemyHealth <= 0) {
         window.alert(enemyName + ' has died!');
@@ -52,11 +51,9 @@ var fight = function(enemyName) {
         window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
       }
   
-      // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
-      console.log(
-        enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
-      );
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
   
       // check player's health
       if (playerHealth <= 0) {
@@ -81,7 +78,7 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     var pickedEnemyName = enemyNames[i];
 
-     enemyHealth = 50;
+     enemyHealth = randomNumber(40, 60);
 
     fight(pickedEnemyName);
 
@@ -101,6 +98,13 @@ for (var i = 0; i < enemyNames.length; i++) {
     }
 
     endGame();
+};
+
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() *(max - min + 1) +min);
+
+    return value;
 };
 
 // function to end the entire game
@@ -128,7 +132,7 @@ var shop = function() {
     var shopOptionPrompt = window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
     );
-};
+
 
 // use switch to carry out action
 switch (shopOptionPrompt) {
@@ -138,8 +142,8 @@ switch (shopOptionPrompt) {
       window.alert("Refilling player's health by 20 for 7 dollars.");
   
       // increase health and decrease money
-      playerHealth = playerHealth + 20;
-      playerMoney = playerMoney - 7;
+      playerHealth = playerHealth += 20;
+      playerMoney = playerMoney -= 7;
         }
         else {
             window.alert("You don't have enough money!");
@@ -153,8 +157,8 @@ switch (shopOptionPrompt) {
       window.alert("Upgrading player's attack by 6 for 7 dollars.");
   
       // increase attack and decrease money
-      playerAttack = playerAttack + 6;
-      playerMoney = playerMoney - 7;
+      playerAttack = playerAttack += 6;
+      playerMoney = playerMoney -= 7;
         }
         else {
             window.alert("You don't have enough money!");
@@ -174,6 +178,8 @@ switch (shopOptionPrompt) {
       shop();
       break;
   }
+};
+
 //start the game when the page loads
 startGame();
 
